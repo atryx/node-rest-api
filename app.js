54589fa9a65f23ce30f9d2cf69f9d6789668b2d7
Mihai Lita <lita.mihai@gmail.com>
2017-02-27 16:28:05 +0200

    Removed node-restful since app already provides endpoint.

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bookmarks = require('./routes/bookmarks.js');
-var restful = require('node-restful');
-var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -32,13 +30,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({type:'appplication/vnd.api+json'}));
-app.use(methodOverride());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
 app.use('/bookmarks', bookmarks);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
