d9ef3b4be0c2911ceea8cd5659dfe2a29f87ff76
Mihai Lita <lita.mihai@gmail.com>
2017-03-02 11:01:45 +0200

    test commit

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use('/bookmarks', bookmarks);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
+  console.log('this is an error');
   err.status = 404;
   next(err);
 });
